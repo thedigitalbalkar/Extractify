032237ccf61d08eb18feafc48bf8cbb340ce60f6
thedigitalbalkar <thedigitalbalkar@gmail.com>
2026-04-17 14:12:19 +0530

    explicit cors config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,15 +4,17 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix("api");
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    })
-  );
+app.enableCors({
+  origin: [
+    "https://extractify-hazel.vercel.app",
+    "http://localhost:5173",
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type, Authorization",
+  credentials: true,
+});
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle("Extractify API")
